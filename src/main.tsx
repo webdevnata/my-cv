af34a0bdcaed43c5c49e3f85e901389ebdfab1b6
Natalia Olow <webdev.nata@gmail.com>
2024-03-17 13:05:08 +0100

    Added basic style scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './style.scss'
 
 interface Experience {
   year: number;
@@ -33,17 +34,17 @@ const cvData: CVData = {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <main>
-      <header>
+    <main className='cv-main'>
+      <header className='cv-header'>
         <h1> CV {cvData.name} {cvData.lastName}</h1>
       </header>
-      <aside>
+      <aside className='cv-personal'>
         <h2>Personal data</h2>
-        <img src={cvData.photo} alt={`${cvData.name} ${cvData.lastName} profile photo `} />
+        <img className='cv-photo' src={cvData.photo} alt={`${cvData.name} ${cvData.lastName} profile photo `} />
         <p>{cvData.name} {cvData.lastName}</p>
         <small> {cvData.position}</small>
       </aside>
-      <section>
+      <section className='cv-details'>
         <h2>Experience</h2>
         <ul>
           {cvData.experience.map(experience => (
